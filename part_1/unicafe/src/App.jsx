c703d0a79f636a55246a9e8a5489cf0b3bb6e7b4
MarkusR73 <markus.rautell@gmail.com>
2025-01-05 11:59:03 +0200

    Solution for exercise 1.6

---
+++ part_1/unicafe/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Header = (props) => {
+  console.log(props)
+  return( 
+  <h1>
+    {props.text}
+  </h1> 
+  )
+}
+
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return(
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header text="give feedback"/>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
+      <Header text="statistics"/>
+      <Display text="good" value={good}/>
+      <Display text="neutral" value={neutral}/>
+      <Display text="bad" value={bad}/>
+    </div>
+  )
+}
+
+export default App
