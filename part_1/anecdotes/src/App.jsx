78179b826b3cd97d387cc3e5270acd9e66d17bb5
MarkusR73 <markus.rautell@gmail.com>
2025-01-05 20:38:03 +0200

    Solution for exercise 1.14

--- part_1/anecdotes/src/App.jsx
+++ part_1/anecdotes/src/App.jsx
@@ -1,5 +1,14 @@
 import { useState } from 'react'
 
+const Header = ({text}) => {
+  return( 
+  <h1>
+    {text}
+  </h1> 
+  )
+}
+
+
 const Button = ({text, onClick}) => {
   return(
     <button onClick={onClick}>
@@ -22,6 +31,8 @@ const App = () => {
 
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
+  const [mostVoted, setMostVoted] = useState(0)
+
   const [selected, setSelected] = useState(0)
 
   const getRandomInt = (max) => {
@@ -35,12 +46,23 @@ const App = () => {
     const newVotes = [...votes];
     // increment the value in position index by one 
     newVotes[index] += 1
-    //Update the state with the new array
+    // Update the state with the new array
     setVotes(newVotes)
+
+    // Update mostVoted with newVotes
+    updateMostVoted(newVotes)
+  }
+
+  const updateMostVoted = (votes) => {
+    // Find the index with the highest vote count
+    const mostVotedIndex = votes.indexOf(Math.max(...votes))
+    // Update state with the index of the most voted anecdote
+    setMostVoted(mostVotedIndex)
   }
 
   return (
     <div>
+      <Header text="Anecdote of the day"/>
       <p>
         {anecdotes[selected]}
       </p>
@@ -49,6 +71,13 @@ const App = () => {
       </p>
       <Button text="vote" onClick={() => vote(selected)}/>
       <Button text="next anecdote" onClick={() => setSelected(getRandomInt(anecdotes.length - 1))}/>
+      <Header text="Anecdote with most votes"/>
+      <p>
+        {anecdotes[mostVoted]}
+      </p>
+      <p>
+        has {votes[mostVoted]} votes
+      </p>
     </div>
   )
 }
