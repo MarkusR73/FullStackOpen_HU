1befa327ce98aa1d8fb73429e05e8245386d697f
MarkusR73 <markus.rautell@gmail.com>
2025-02-04 20:42:55 +0200

    Solution for exercise 5.3

--- part_5/bloglist-frontend/src/App.jsx
+++ part_5/bloglist-frontend/src/App.jsx
@@ -7,6 +7,9 @@ import { LoginForm, BlogForm } from './Forms'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+	const [title, setTitle] = useState([])
+	const [author, setAuthor] = useState([])
+	const [url, setUrl] = useState([])
 	const [username, setUsername] = useState('') 
   const [password, setPassword] = useState('') 
 	const [user, setUser] = useState(null)
@@ -31,9 +34,7 @@ const App = () => {
     event.preventDefault()
     
     try {
-      const user = await loginService.login({
-        username, password,
-      })
+      const user = await loginService.login({ username, password })
 			window.localStorage.setItem(
         'loggedNoteappUser', JSON.stringify(user)
       ) 
@@ -54,6 +55,29 @@ const App = () => {
 		setUser(null)
 	}
 
+	const createBlog = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const newBlog = await blogService.create({ title, author, url })
+			setBlogs(blogs.concat(newBlog))
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } 
+		catch (exception) {
+			if (exception.response) {
+				const { status, data } = exception.response
+				setErrorMessage(`Error ${status}: ${data.error}`)
+			} else {
+				setErrorMessage('An unexpected error occurred')
+			}
+			setTimeout(() => {
+				setErrorMessage(null)
+			}, 5000)
+    }
+	}
+
   return (
     <div>
 			<Notification message={errorMessage} />
@@ -70,7 +94,15 @@ const App = () => {
 						{user.name} logged in
 						<button onClick={handleLogout}>logout</button>
 					</p>
-					<BlogForm />
+					<BlogForm 
+						createBlog={createBlog}
+						title={title}
+						setTitle={setTitle}
+						author={author}
+						setAuthor={setAuthor}
+						url={url}
+						setUrl={setUrl}
+					/>
 					{blogs.map(blog =>
         		<Blog key={blog.id} blog={blog} />
       		)}
