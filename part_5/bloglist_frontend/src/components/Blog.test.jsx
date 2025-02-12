34ec0e330b90a20a9dee9d431ea19639521e75f7
MarkusR73 <markus.rautell@gmail.com>
2025-02-12 18:23:34 +0200

    Solution for exercise 5.14

--- part_5/bloglist_frontend/src/components/Blog.test.jsx
+++ part_5/bloglist_frontend/src/components/Blog.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('Renders only blog`s summary elements', () => {
+describe('<Blog />', () => {
+  let container
   const blog = {
     title: 'Teppo testaa blog summarya',
     author: 'Teppo Testaaja',
@@ -10,15 +12,35 @@ test('Renders only blog`s summary elements', () => {
     user: { name: 'Matti Näsä' }
   }
 
-  const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(<Blog blog={blog} />).container
+  })
 
-  // Check that title, author and view button are visible
-  expect(container.querySelector('.blog-title')?.textContent).toBe('Teppo testaa blog summarya')
-  expect(container.querySelector('.blog-author')?.textContent).toBe('Teppo Testaaja')
-  expect(container.querySelector('.toggle-visibility-btn')?.textContent).toBe('view')
+  test('Renders only blog`s summary elements', () => {
+    // Check that title, author and view button are visible
+    expect(container.querySelector('.blog-title')?.textContent).toBe('Teppo testaa blog summarya')
+    expect(container.querySelector('.blog-author')?.textContent).toBe('Teppo Testaaja')
+    expect(container.querySelector('.toggle-visibility-btn')?.textContent).toBe('view')
 
-  // Ensure that URL, likes and user are not visible
-  expect(container.querySelector('.blog-url')).toBeNull()
-  expect(container.querySelector('.blog-likes')).toBeNull()
-  expect(container.querySelector('.blog-user')).toBeNull()
+    // Ensure that URL, likes and user are not visible
+    expect(container.querySelector('.blog-url')).toBeNull()
+    expect(container.querySelector('.blog-likes')).toBeNull()
+    expect(container.querySelector('.blog-user')).toBeNull()
+  })
+
+  test('Renders all details when view button is clicked', async () => {
+
+    const user = userEvent.setup()
+    const viewButton = container.querySelector('.toggle-visibility-btn')
+
+    await user.click(viewButton)
+
+    expect(container.querySelector('.blog-title')?.textContent).toBe('Teppo testaa blog summarya')
+    expect(container.querySelector('.blog-author')?.textContent).toBe('Teppo Testaaja')
+    expect(container.querySelector('.blog-url')?.textContent).toBe('www.tepi-testaa.fi')
+    // Avoids issues with the like-button text
+    expect(container.querySelector('.blog-likes')?.textContent).toContain('Likes: 0')
+    expect(container.querySelector('.blog-user')?.textContent).toBe('Matti Näsä')
+    expect(container.querySelector('.toggle-visibility-btn')?.textContent).toBe('hide')
+  })
 })
