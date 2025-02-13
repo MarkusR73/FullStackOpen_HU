7e3ed609c4ab837b51d854130a7bb909906559c3
MarkusR73 <markus.rautell@gmail.com>
2025-02-13 19:04:41 +0200

    Solution for exercise 5.18

--- part_5/playwright_tests/tests/blog_app.spec.js
+++ part_5/playwright_tests/tests/blog_app.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
+//npm run test:report
+
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Teppo Testaaja',
+        username: 'TestiTepi88',
+        password: 'T€nttu88'
+      }
+    })
     await page.goto('http://localhost:5173')
   })
 
@@ -9,12 +19,38 @@ describe('Blog app', () => {
     const header = await page.getByText('log in to application')
     await expect(header).toBeVisible()
 
-    const username_field = await page.getByLabel('Username')
-    const password_field = await page.getByLabel('Password')
-    await expect(username_field).toBeVisible
-    await expect(password_field).toBeVisible
+    await expect(page.locator('[name="Username"]')).toBeVisible()
+    await expect(page.locator('[name="Password"]')).toBeVisible()
 
     const login_button = await page.getByRole('button', { name: 'Login' })
     await expect(login_button).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await expect(page.getByText('log in to application')).toBeVisible()
+
+      await expect(page.locator('[name="Username"]')).toBeVisible()
+      await page.locator('[name="Username"]').fill('TestiTepi88')
+      await expect(page.locator('[name="Password"]')).toBeVisible()
+      await page.locator('[name="Password"]').fill('T€nttu88')
+
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Teppo Testaaja logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await expect(page.getByText('log in to application')).toBeVisible()
+
+      await expect(page.locator('[name="Username"]')).toBeVisible()
+      await page.locator('[name="Username"]').fill('TestiTepi89')
+      await expect(page.locator('[name="Password"]')).toBeVisible()
+      await page.locator('[name="Password"]').fill('T€nttu89')
+
+      await page.getByRole('button', { name: 'Login' }).click()
+
+      await expect(page.getByText('Wrong username or password!')).toBeVisible()
+    })
   })
 })
