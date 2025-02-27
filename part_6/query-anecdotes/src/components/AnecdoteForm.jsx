176829172fed70d381c215df12a1431ce333a928
MarkusR73 <markus.rautell@gmail.com>
2025-02-27 16:57:29 +0200

    Solution for exercise 6.24

--- part_6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part_6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -12,16 +12,16 @@ const AnecdoteForm = () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
       dispatch({ type: 'SET_NOTIFICATION', payload: `Anecdote '${newAnecdote.content}' created!` })
       setTimeout(() => dispatch({ type: 'CLEAR_NOTIFICATION' }), 5000)
-    } 
+    },
+    onError: (error) => {
+			dispatch({ type: 'SET_NOTIFICATION', payload: `Error: ${error.response?.data?.error || 'Failed to add anecdote'}` })
+			setTimeout(() => dispatch({ type: 'CLEAR_NOTIFICATION' }), 5000)
+		} 
   })
 
   const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    if (content.length < 5) {
-      alert('Anecdote must be at least 5 characters long!')
-      return
-    }
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0})
   }
